2fa2659f3adbe1f40b14c23e461ac33498ba7374
Mike Sasiela <m5455y@gmail.com>
2021-03-11 19:18:17 -0600

    added compression

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require('express')
+const ffmpeg = require('ffmpeg')
+var compression = require('compression')
 
-const htmlRouter = require('./routes/html-routes.js');
+const htmlRouter = require('./routes/html-routes.js')
 
 // Sets up the Express App
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 // Static directory
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Invoke routes
-htmlRouter(app);
+htmlRouter(app)
 
 // Starts our Express app
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
